cd82af9050a60e58ff0833212fc14cd1e1b24ab8
Lea Gundersen <louylou@fullsail.edu>
2013-12-19 13:45:39 -0500

    Created a new firebase. added new links to new firebase in core.js

--- assets/js/ctrl/core.js
+++ assets/js/ctrl/core.js
@@ -1,10 +1,11 @@
-<!-- core.js -->
+<!-- all controllers -->
+
 
 app.controller('GlobalCtrl', ['$scope', '$firebase', function( $scope, $firebase ){
 		
 		$scope.list;
 		
-        var url = 'https://jotitdownapp.firebaseio.com/'; //link to my firebase account
+        var url = 'https://mymddapp.firebaseio.com/'; //link to my firebase account
         var sync = $firebase(new Firebase(url)).$bind($scope, 'ListStorage'); 
         
         $scope.addList = function() {
@@ -18,7 +19,7 @@ app.controller('GlobalCtrl', ['$scope', '$firebase', function( $scope, $firebase
 //controller for AllLists.html
 app.controller('AllListsCtrl', ['$scope', '$firebase', function( $scope, $firebase ){ 
 	
-        var url = 'https://jotitdownapp.firebaseio.com/'; 
+        var url = 'https://mymddapp.firebaseio.com/'; 
         var sync = $firebase(new Firebase(url)).$bind($scope, 'ListStorage'); 
         
         $scope.titleList = function() { 
@@ -53,10 +54,10 @@ app.controller('ListDetail', ['$scope', '$firebase', '$routeParams', '$location'
 		}
 		
 		$scope.ListStorage[$scope.listId].toDos.push($scope.newTask);
-		$scope.newTask = "";
+		$scope.newTask = ""; //sets the input field back to blank
 	}
 	
-	//deletes the task that has been clicked
+	//deletes the task that has been clicked on
 	$scope.toggleTask = function(taskId) {
 		
 		delete $scope.ListStorage[$scope.listId].toDos[taskId];
@@ -67,15 +68,37 @@ app.controller('ListDetail', ['$scope', '$firebase', '$routeParams', '$location'
 	$scope.deleteList = function() {
 			
 		$scope.ListStorage.$remove($scope.listId);
+		//$scope.ListStorage.$save(); //idk if this is needed
 		
 		//once items deleted will send the user back to 'all Lists' page
 		$location.path('/allLists');	
 	}
 }]);
 
+
+app.controller('loginCtrl', ['$scope', '$firebase', '$location', function( $scope, $firebase, $location ){
+
+        var url = 'https://mymddapp.firebaseio.com/'; //link to my firebase account
+        var sync = $firebase(new Firebase(url)).$bind($scope, 'ListStorage'); 
+        
+        $scope.username = null;
+        $scope.password = null;
+        
+        $scope.login = function() {
+        	
+        	var user = {
+        		"username": $scope.user, 
+				"password": $scope.pass 
+        	}
+        	$scope.ListStorage.$add(user); 
+        }
+
+
+}]);
+
 app.controller('MembersCtrl', ['$scope', '$firebase', function( $scope, $firebase ){
 
-        var url = 'https://jotitdownapp.firebaseio.com/'; //link to my firebase account
+        var url = 'https://mymddapp.firebaseio.com/'; 
         var sync = $firebase(new Firebase(url)).$bind($scope, 'ListStorage'); 
         
        // $scope.username = null;
@@ -92,60 +115,7 @@ app.controller('MembersCtrl', ['$scope', '$firebase', function( $scope, $firebas
         	}
         	$scope.ListStorage.$add(newMember); 
         }
-        
-
 }]);
-
-app.controller('loginCtrl', ['$scope', '$firebase', function( $scope, $firebase ){
-
-        var url = 'https://jotitdownapp.firebaseio.com/'; //link to my firebase account
-        var sync = $firebase(new Firebase(url)).$bind($scope, 'ListStorage'); 
-        
-        $scope.username = null;
-        $scope.password = null;
-        
-        $scope.login = function() {
-        	
-        	var user = {
-        		"username": $scope.user, 
-				"password": $scope.pass 
-        	}
-        	$scope.ListStorage.$add(user); 
-        }
-        
-/*
-	// instatiate the FirebaseSimpleLogin and monitor the user's auth state
-	var chatRef = new Firebase('https://jotitdownapp.firebaseio.com/');
-	var auth = new FirebaseSimpleLogin(chatRef, function(error, user) {
-	  if (error) {
-		// an error occurred while attempting login
-		alert(error);
-	  } else if (user) {
-		// user authenticated with Firebase
-		alert('User ID: ' + user.id + ', Provider: ' + user.provider);
-	  } else {
-		// user is logged out
-	  }
-	});
-
-	// attempt to log the user in with your preferred authentication provider
-	auth.login('<provider>'); //make it facebook
-	
-	
-	
-	
-	auth.login('facebook', {
-	  //access_token: ,
-	  rememberMe: true,
-	  scope: 'email,user_likes'
-	});
-
-	
-*/
-        
-
-}]);
-
 
 
 
